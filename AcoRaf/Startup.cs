6656d7dbebcf01c82a27d2b36b7a0e0fc1092480
churko <kuschnire@gmail.com>
2017-10-03 16:09:02 -0300

    Add project files.

---
+++ AcoRaf/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AcoRaf.Startup))]
+namespace AcoRaf
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
